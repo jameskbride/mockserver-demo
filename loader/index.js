b583db49c244ad58f55aec478686401316d2cac8
Jim Kirkbride <james.kirkbride@gmail.com>
2017-09-30 15:23:00 -0400

    Adding GET for individual record.  Adding additional metadeta to support individual records.

--- loader/index.js
+++ loader/index.js
@@ -6,29 +6,33 @@ const path = require('path');
 let mockServerClient = mockServer.mockServerClient;
 const jsonFiles = glob.sync(path.resolve(__dirname, './src/expectations/') + '/*.json');
 
-jsonFiles.forEach(expectation => {
-        const jsonExpectation = JSON.parse(fs.readFileSync(expectation));
-        mockServerClient("localhost", 9200).mockAnyResponse(
-            {
-                'httpRequest': {
-                    'method': jsonExpectation.method,
-                    'path': jsonExpectation.path,
-                    'queryStringParameters': jsonExpectation.queryStringParameters
-                },
-                'httpResponse': {
-                    'statusCode': jsonExpectation.statusCode,
-                    'body': JSON.stringify(jsonExpectation.body),
-                    'headers': [
-                        {
-                            "name": "Content-Type",
-                            "values": ["application/json; charset=utf-8"]
-                        }
-                    ]
-                },
-                'times': {
-                    'unlimited': true
+let loadExpectation = (expectation) => {
+    const jsonExpectation = JSON.parse(fs.readFileSync(expectation));
+    let preProcessedExpectation = {
+        'httpRequest': {
+            'method': jsonExpectation.method,
+            'path': jsonExpectation.path
+        },
+        'httpResponse': {
+            'statusCode': jsonExpectation.statusCode,
+            'body': JSON.stringify(jsonExpectation.body),
+            'headers': [
+                {
+                    "name": "Content-Type",
+                    "values": ["application/json; charset=utf-8"]
                 }
-            }
-        )
+            ]
+        },
+        'times': {
+            'unlimited': true
+        }
+    };
+
+    if (jsonExpectation.queryStringParameters) {
+        preProcessedExpectation.httpRequest.queryStringParameters =  jsonExpectation.queryStringParameters;
     }
-);
+
+    mockServerClient("localhost", 9200).mockAnyResponse(preProcessedExpectation);
+};
+
+jsonFiles.forEach(expectation => loadExpectation(expectation));
